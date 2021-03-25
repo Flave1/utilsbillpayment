729c972c20396a2d65fc8f1d4a012977bbf8fb28
dikshaemizentech <gayatri.agrawal56@gmail.com>
2021-03-25 16:16:41 +0530

    commit global.asax file

--- VendTech/Global.asax.cs
+++ VendTech/Global.asax.cs
@@ -1,14 +1,14 @@
 ﻿using Quartz;
 using Quartz.Impl;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Security.Cryptography;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using VendTech.App_Start;
+using VendTech.BLL.Models;
 
 namespace VendTech
 {
@@ -36,7 +36,22 @@ namespace VendTech
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-           
+
+        }
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            var error = Server.GetLastError();
+            var cryptoEx = error as CryptographicException;
+            if (cryptoEx != null)
+            {
+                HttpCookie auth_cookie = Request.Cookies[Cookies.AdminAuthorizationCookie];
+                if (auth_cookie != null)
+                {
+                    auth_cookie.Expires = DateTime.Now.AddDays(-30);
+                    Response.Cookies.Add(auth_cookie);
+                }
+                Server.ClearError();
+            }
         }
     }
 }
