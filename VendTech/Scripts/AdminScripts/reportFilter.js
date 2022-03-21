5b853bd306e023e0676224afc2fe0ada1cfc4d90
flave1 <favouremmanuel433@gmail.com>
2022-03-21 09:29:52 +0100

    ...

---
+++ VendTech/Scripts/AdminScripts/reportFilter.js
@@ -0,0 +1,147 @@
+﻿
+$(document).ready(function () { 
+    var urlParams = new URLSearchParams(window.location.search);
+    if (urlParams == "") {
+        $("#FromDate").kendoDatePicker({
+            culture: "en-GB",
+            value: new Date()
+        });
+        $("#ToDate").kendoDatePicker({
+            culture: "en-GB",
+            value: new Date()
+        });
+    } else {
+        let from = urlParams.get("from");
+        let to = urlParams.get("to");
+        let transId = urlParams.get("transactionId");
+        let meter = urlParams.get("meter");
+        let pos = urlParams.get("pos");
+        let vendorId = urlParams.get("vendorId");
+        let type = urlParams.get("type");
+
+        $("#reportType").val(type); 
+        $("#meterNo").val(meter);
+        $("#vendor").val(vendorId);
+        $("#tranId").val(transId);
+        $("#pos").val(pos);
+        $("#FromDate").kendoDatePicker({
+            culture: "en-GB",
+            value: new Date(from),
+            format: "dd/MM/yyyy"
+        });
+        $("#ToDate").kendoDatePicker({
+            culture: "en-GB",
+            value: new Date(to),
+            format: "dd/MM/yyyy"
+        });
+    }
+    $("#fromSpan").text($("#FromDate").val())
+    $("#toSpan").text($("#ToDate").val())
+    $("#printedDate").text($("#ToDate").val())
+
+
+    var d = new Date();
+    $("#FromDate").kendoDatePicker({
+        max: new Date(d.getFullYear(), d.getMonth(), d.getDate()),
+        format: "dd/MM/yyyy"
+    });
+    var datePicker1 = $("#FromDate").data("kendoDatePicker");
+    $("#FromDate").click(function () {
+        datePicker1.open();
+    })
+    $("#ToDate").kendoDatePicker({
+        max: new Date(d.getFullYear(), d.getMonth(), d.getDate()),
+        format: "dd/MM/yyyy"
+    });
+    var datePicker2 = $("#ToDate").data("kendoDatePicker");
+    $("#ToDate").click(function () {
+        datePicker2.open();
+    })
+
+    $("#reportType").on("change", function () {
+
+        var type = $("#reportType").val();
+        var vendorId = $('#vendor').val();
+        var pos = $('#pos').val();
+        var meterNo = $('#meterNo').val();
+        var transactionId = $('#tranId').val();
+
+        var From = $('#FromDate').val();
+        if (From) {
+            var val = From.split("/");
+            From = val[1] + "/" + val[0] + "/" + val[2];
+        }
+        var To = $('#ToDate').val();
+        if (To) {
+            var val = To.split("/");
+            To = val[1] + "/" + val[0] + "/" + val[2];
+        }
+
+        if (transactionId == undefined) { 
+            transactionId = ''
+        }
+
+        if (meterNo == undefined) { 
+            meterNo = ''
+        }
+
+        if (vendorId == undefined) {
+            vendorId = ''
+        }
+
+        if (pos == undefined) {
+            pos = 0
+        }
+         
+
+        window.location.href = "/Admin/Report/ManageReports?type=" + type + "&vendorId=" + vendorId + "&pos=" + pos + "&meter=" + meterNo + "&transactionId=" + transactionId + "&from=" + From + "&to=" + To;
+    });
+
+    var d = new Date();
+    $("#value_date").kendoDatePicker({
+        culture: "en-GB",
+        value: new Date(d.getFullYear(), d.getMonth() + 2, d.getDate())
+    });
+
+
+    $("#value_date").kendoDatePicker({
+
+        max: new Date(d.getFullYear(), d.getMonth(), d.getDate()),
+        format: "dd/MM/yyyy hh:mm",
+        max: new Date(d.setDate(d.getDate() + 365)),
+        maxDate: new Date, minDate: new Date(2007, 6, 12),
+    });
+    var datePicker3 = $("#value_date").data("kendoDatePicker");
+    $("#value_date").click(function () {
+        datePicker3.open();
+    });
+
+
+    $("#vendor").on("change", function () {
+        var vendorId = $("#vendor").val();
+        $("#pos").empty();
+        if (vendorId) {
+            $.ajax({
+                url: '/Admin/Report/GetVendorPosSelectList?userId=' + vendorId,
+                success: function (res) {
+                    if (res.posList.length > 1) {
+                        $("#pos").append("<option value=''> -Select POS- </option>")
+                    }
+                    if (res.posList.length == 0) {
+                        $("#pos").append("<option value=''> -NO POS - </option>")
+                    }
+                    if (res.posList != null) {
+                        for (var i = 0; i < res.posList.length; i++) {
+                            $("#pos").append("<option value=" + res.posList[i].Value + ">" + res.posList[i].Text + "</option>")
+                        }
+                    }
+                }
+            })
+        }
+        else {
+            $("#pos").append("<option value=''> SELECT POS</option>")
+        }
+    });
+     
+});
+
---
+++ VendTech/Scripts/UserScripts/reportFilter.js
@@ -0,0 +1,122 @@
+﻿
+ 
+
+function changeReport(type) {
+    
+    var pos = $('#posDrp').val();
+    var meterNo = $('#meterNo').val();
+    var transactionId = $('#tranId').val(); 
+
+    var From = $('#FromDate').val();
+    if (From) {
+        var val = From.split("/");
+        From = val[1] + "/" + val[0] + "/" + val[2];
+    }
+    var To = $('#ToDate').val();
+    if (To) {
+        var val = To.split("/");
+        To = val[1] + "/" + val[0] + "/" + val[2];
+    }
+     
+    if (transactionId == undefined) {
+        transactionId = ''
+    }
+
+    if (meterNo == undefined) {
+        meterNo = ''
+    }
+     
+
+    if (pos == undefined) {
+        pos = 0
+    }
+
+    if (type == 1)
+        window.location.href = "/Report/DepositReport?pos=" + pos + "&meter=" + meterNo + "&transactionId=" + transactionId + "&from=" + From + "&to=" + To + "&type=" + type
+    else if (type == 3)
+        window.location.href = "/Report/BalanceSheetReport?pos=" + pos + "&meter=" + meterNo + "&transactionId=" + transactionId + "&from=" + From + "&to=" + To + "&type=" + type
+    else if (type == 4)
+        window.location.href = "/Report/GSTSalesReport?pos=" + pos + "&meter=" + meterNo + "&transactionId=" + transactionId + "&from=" + From + "&to=" + To + "&type=" + type
+    else if (type == 5)
+        window.location.href = "/Report/AgentRevenueReport?pos=" + pos + "&meter=" + meterNo + "&transactionId=" + transactionId + "&from=" + From + "&to=" + To + "&type=" + type
+    else
+        window.location.href = "/Report/SalesReport?pos=" + pos + "&meter=" + meterNo + "&transactionId=" + transactionId + "&from=" + From + "&to=" + To + "&type=" + type
+}
+
+$(document).ready(function () {
+
+
+    var d = new Date();
+    $("#FromDate").kendoDatePicker({
+        max: new Date(d.getFullYear(), d.getMonth(), d.getDate()),
+        format: "dd/MM/yyyy"
+    });
+    var datePicker1 = $("#FromDate").data("kendoDatePicker");
+    $("#FromDate").click(function () {
+        datePicker1.open();
+    });
+    $("#ToDate").kendoDatePicker({
+        max: new Date(d.getFullYear(), d.getMonth(), d.getDate()),
+        format: "dd/MM/yyyy"
+    });
+    var datePicker2 = $("#ToDate").data("kendoDatePicker");
+    $("#ToDate").click(function () {
+        datePicker2.open();
+    });
+
+     
+
+
+    const date = new Date();
+    const formattedDate = date.toLocaleDateString('en-GB', {
+        day: '2-digit', month: '2-digit', year: 'numeric'
+    }).replace(/ /g, '-') + " " + date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }).replace("AM", "").replace("PM", "");
+    // $("#printedDate").text(printDt.getDate() + "/" + getMonthName(printDt.getMonth()) + "/" + printDt.getFullYear()+" "+printDt.toLocaleTimeString());
+    $("#printedDate").text(formattedDate);
+    $("#PrintedDateServer").val(formattedDate);
+
+     
+    var urlParams = new URLSearchParams(window.location.search);
+    if (urlParams == "") {
+
+        $("#FromDate").kendoDatePicker({
+            culture: "en-GB",
+            value: new Date(),
+            change: function () { $("#fromSpan").html($("#FromDate").val()) }
+        });
+        $("#ToDate").kendoDatePicker({
+            culture: "en-GB",
+            value: new Date(),
+            change: function () { $("#toSpan").html($("#ToDate").val()) }
+        });
+
+        $("#fromSpan").text($("#FromDate").val())
+        $("#toSpan").text($("#ToDate").val())
+    } else {
+        let from = urlParams.get("from");
+        let to = urlParams.get("to");
+        let transId = urlParams.get("transId");
+        let meter = urlParams.get("meter");
+        let pos = urlParams.get("pos");
+        let type = urlParams.get("type");
+        $("#fromSpan").html(from)
+        $("#toSpan").html(to);
+        $("#rpt").val(type);
+        $("#FromDate").kendoDatePicker({
+            culture: "en-GB",
+            value: new Date(from),
+            format: "dd/MM/yyyy",
+            change: function () { $("#fromSpan").html($("#FromDate").val()) }
+        });
+        $("#ToDate").kendoDatePicker({
+            culture: "en-GB",
+            value: new Date(to),
+            format: "dd/MM/yyyy",
+            change: function () { $("#toSpan").html($("#ToDate").val()) }
+        });
+
+
+
+    }
+
+});
