2e5ade0df2271dbc4d261a206b29b2eb8b0e1277
flave1 <favouremmanuel433@gmail.com>
2022-11-08 13:38:50 +0100

    ...

---
+++ VendTech/Controllers/PrivacyController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using VendTech.BLL.Interfaces;
+using VendTech.BLL.Models;
+
+namespace VendTech.Controllers
+{
+    public class PrivacyController: Controller
+    {
+
+        private readonly ICMSManager _cmsManager;
+        public PrivacyController(ICMSManager cmsManager)
+        {
+            _cmsManager = cmsManager;
+        }
+        public ActionResult Policy()
+        {
+            CMSPageViewModel model = _cmsManager.GetPageContentByPageIdforFront(2);
+            return View(model);
+        }
+    }
+}
