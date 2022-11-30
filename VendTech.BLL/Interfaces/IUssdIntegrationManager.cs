3a5dfd4b3ffea8853c98e66604c38e74312be06d
flave1 <favouremmanuel433@gmail.com>
2022-11-30 08:52:13 +0100

    ..

---
+++ VendTech.BLL/Interfaces/IUssdIntegrationManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendTech.BLL.Interfaces
+{
+    public interface IUssdIntegrationManager
+    {
+    }
+}
---
+++ VendTech.BLL/Managers/UssdIntegrationManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VendTech.BLL.Interfaces;
+
+namespace VendTech.BLL.Managers
+{
+    public class UssdIntegrationManager : IUssdIntegrationManager
+    {
+        private readonly IPOSManager _posManager;
+        public UssdIntegrationManager(IPOSManager posManager)
+        {
+            _posManager = posManager;
+            //_posManager.DeductFromVendorPOSBalance;
+            //_posManager.keepTransanctiondetails
+        }
+
+    }
+}
