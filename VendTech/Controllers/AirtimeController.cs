f9354d3de9c95a9f7250759ebad0e41f4cc27c4f
flave1 <favouremmanuel433@gmail.com>
2022-12-06 06:25:34 +0100

    some update

---
+++ VendTech/Controllers/AirtimeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using VendTech.Areas.Admin.Controllers;
+using VendTech.BLL.Interfaces;
+
+namespace VendTech.Controllers
+{
+    public class AirtimeController: AppUserBaseController
+    {
+        public AirtimeController( IErrorLogManager errorLogManager): base(errorLogManager)
+        {
+        }
+
+        public ActionResult Recharge()
+        {
+            return View();
+        }
+    }
+}
