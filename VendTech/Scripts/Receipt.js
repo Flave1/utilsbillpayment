799c0b90d59e4ba2c1f009d1747a1a2adcda328f
flave1 <favouremmanuel433@gmail.com>
2023-04-30 05:55:02 +0100

    hj

--- VendTech/Scripts/Receipt.js
+++ VendTech/Scripts/Receipt.js
@@ -12,7 +12,7 @@ function onViewDepositDetails(depositId) {
 
                 $('.modal-body').html(data);
                 $("#depositDetailModal").modal("show");
-                
+
             }
         });
     }
@@ -21,22 +21,22 @@ function onViewDepositDetails(depositId) {
 
 
 function GetRequestANDResponse(transactionId) {
-   
+
     DisableAndEnablelinks(true, transactionId);
 
     try {
 
         var inputParam = new Object();
-        inputParam.token_string = transactionId.replace(/{{{/g, '').replace(/}}}/g, ''); 
+        inputParam.token_string = transactionId.replace(/{{{/g, '').replace(/}}}/g, '');
 
-     
+
 
         $.ajax({
             url: baseUrl + '/Meter/ReturnRequestANDResponseJSON',
             data: $.postifyData(inputParam),
             type: "POST",
             success: function (data) {
-               
+
 
                 DisableAndEnablelinks(false, transactionId);
 
@@ -46,7 +46,7 @@ function GetRequestANDResponse(transactionId) {
                 console.log(data);
 
                 document.getElementById("Request").textContent = JSON.stringify(request, undefined, 2);
-                document.getElementById("Response").textContent = JSON.stringify(respone, undefined, 2); 
+                document.getElementById("Response").textContent = JSON.stringify(respone, undefined, 2);
                 $("#modalCart3").modal("show");
 
 
@@ -60,8 +60,77 @@ function GetRequestANDResponse(transactionId) {
 
 }
 
+function fetchSaleInformation(token, platFormId) {
+
+    switch (platFormId) {
+        case '1':
+            fetchVoucherDetailsByToken(token);
+            break;
+        default:
+            fetchAirtimeDetailsByTransactionId(token);
+    }
+}
+
+function fetchAirtimeDetailsByTransactionId(traxId) {
+
+    DisableAndEnablelinks(true, traxId);
+    $("#re-print_section").show();
+    try {
+
+        var inputParam = new Object();
+        inputParam.id = traxId.split('/')[1];
+
+        $.ajax({
+            url: baseUrl + '/Airtime/GetAirtimeReceipt',
+            data: $.postifyData(inputParam),
+            type: "POST",
+            success: function (data) {
+
+                const response = JSON.parse(data)
+                DisableAndEnablelinks(false, traxId);
+
+
+                if (response.Code === 302) {
+                    $.ShowMessage($('div.messageAlert'), response.Msg, MessageType.Failed);
+                    return false;
+                }
+
+                if (response.Code === 200) {
+
+                    $("#sales_date").html(response.Data.TransactionDate);
+                    $("#customer_name").html(response.Data.CustomerName);
+                    $("#customer_account_number").html(response.Data.Phone);
+                    $("#amount_tender").html(response.Data.Amount);
+                    $("#service_charge").html(response.Data.Charges);
+                    $("#debit_recovery").html(response.Data.DebitRecovery);
+                    $("#cost_of_units").html(response.Data.UnitCost);
+                    $("#units").html(response.Data.Unit);
+                    $("#pin1").html(response.Data.Pin1);
+                    $("#edsa_serial").html(response.Data.SerialNo);
+                    $("#barcode").html(response.Data.Phone);
+                    $("#vendtech_serial_code").html(response.Data.VTECHSerial);
+                    $("#pos_id").html(response.Data.POS);
+                    if (response.Data.ShouldShowSmsButton) $("#showsms_btn").show();
+                    if (response.Data.ShouldShowPrintButton) $("#showprint_btn").show();
+                    $("#vendorId").html(response.Data.VendorId);
+                    $("#receiptTitle").html(response.Data.ReceiptTitle);
+
+
+                    $("#airtimeReceiptModal").modal("show");
+                } else {
+
+                    $.ShowMessage($('div.messageAlert'), response.Msg, MessageType.Failed);
+                }
+            },
+        });
+
+    } catch (e) {
+        DisableAndEnablelinks(false, traxId);
+    }
+}
+
 function fetchVoucherDetailsByToken(token) {
-    
+
     DisableAndEnablelinks(true, token);
     $("#re-print_section").show();
     try {
@@ -75,7 +144,7 @@ function fetchVoucherDetailsByToken(token) {
             data: $.postifyData(inputParam),
             type: "POST",
             success: function (data) {
-               
+
 
                 DisableAndEnablelinks(false, token);
                 if (data.Code === 302) {
@@ -87,7 +156,7 @@ function fetchVoucherDetailsByToken(token) {
                 if (data.Code === 200) {
 
                     console.log(data);
-                    
+
                     $("#re-customer_name").html(data.Data.CustomerName);
                     $("#re-customer_account_number").html(data.Data.AccountNo);
                     $("#re-customer_address").html(data.Data.Address);
@@ -113,7 +182,7 @@ function fetchVoucherDetailsByToken(token) {
                     $("#re-vendorId").html(data.Data.VendorId);
                     if (data.Data.ShouldShowSmsButton) $("#re-showsms_btn").show();
                     if (data.Data.ShouldShowPrintButton) $("#re-showprint_btn").show();
-                   
+
                     $("#modalCart2").modal("show");
                 } else {
 
@@ -130,7 +199,7 @@ function fetchVoucherDetailsByToken(token) {
 }
 
 function DisableAndEnablelinks(enable, token) {
-   
+
     if (enable) {
         $("td a").text(function (index, text) {
             return text.replace(token, "please wait......");
@@ -147,10 +216,10 @@ function DisableAndEnablelinks(enable, token) {
 
 
 function JsonPrint() {
-   
+
     $("#json_print_section").hide();
     var prtContent = document.getElementById("requestRequestContent");
-    var WinPrint = window.open('', '', 'left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0');  
+    var WinPrint = window.open('', '', 'left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0');
     WinPrint.document.write('<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">');
 
     WinPrint.document.write(prtContent.innerHTML);
@@ -162,14 +231,14 @@ function JsonPrint() {
 
 
 function Reprint() {
-   
+
     $("#re-print_section").hide();
     var prtContent = document.getElementById("re-printSection");
     var WinPrint = window.open('', '', 'left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0');
     WinPrint.document.write('<link href="~/Content/pos_receipt.css" rel="stylesheet" />');
     WinPrint.document.write('<link href="https://fonts.googleapis.com/css2?family=Libre+Barcode+39+Text&display=swap" rel="stylesheet">');
     WinPrint.document.write('<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">');
- 
+
     WinPrint.document.write(prtContent.innerHTML);
     WinPrint.document.close();
     WinPrint.focus();
@@ -178,10 +247,10 @@ function Reprint() {
 }
 
 function depositReprint() {
-    
+
     $("#re-print_section").hide();
     var prtContent = document.getElementById("re-printSection");
-    var WinPrint = window.open('', '', 'left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0'); 
+    var WinPrint = window.open('', '', 'left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0');
     WinPrint.document.write('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">');
 
     WinPrint.document.write(prtContent.innerHTML);
@@ -192,7 +261,7 @@ function depositReprint() {
 }
 
 function Print() {
-   
+
     $("#print_section").hide();
     var prtContent = document.getElementById("printSection");
     var WinPrint = window.open('', '', 'left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0');
