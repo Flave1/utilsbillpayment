b8a4e854861703a421a56fc6be8675d569e0d861
flavetechs <104333668+flavetechs@users.noreply.github.com>
2023-12-08 08:03:02 +0100

    as

--- VendTech/Controllers/TransferController.cs
+++ VendTech/Controllers/TransferController.cs
@@ -239,7 +239,7 @@ namespace VendTech.Controllers
                     body = body.Replace("%otp%", result.Object);
                     body = body.Replace("%USER%", LOGGEDIN_USER.FirstName);
                     var currentUser = LOGGEDIN_USER.UserID;
-                    //Utilities.SendEmail(User.Identity.Name, emailTemplate.EmailSubject, body);
+                    Utilities.SendEmail(User.Identity.Name, emailTemplate.EmailSubject, body);
                 }
 
                 var user = _userManager.GetAppUserProfile(LOGGEDIN_USER.UserID);
