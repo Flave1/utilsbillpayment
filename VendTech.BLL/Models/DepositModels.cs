2859c4d10947976e9a81834b52fb248d22ec7430
flavetechs <104333668+flavetechs@users.noreply.github.com>
2024-01-06 05:53:36 +0100

    sd

--- VendTech.BLL/Models/DepositModels.cs
+++ VendTech.BLL/Models/DepositModels.cs
@@ -2,10 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Globalization;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Web.Mvc;
 using VendTech.BLL.Common;
 using VendTech.DAL;
