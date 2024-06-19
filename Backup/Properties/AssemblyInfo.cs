3fd973bd53bb044461e17e94e9ac3980d8463a52
flavetechs <104333668+flavetechs@users.noreply.github.com>
2024-06-19 10:20:59 +0100

    test

---
+++ Backup/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Vendtech.TEST")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Vendtech.TEST")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("246532a3-8784-49ea-b170-b775577443f3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ VendTech/Areas/Api/Controllers/B2bTestController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using VendTech.DAL;
+
+namespace VendTech.Areas.Api.Controllers.b2b
+{
+    public class b2btestController: ApiController
+    {
+        private readonly ICalculatorService _calculatorService;
+
+        public b2btestController(ICalculatorService calculatorService)
+        {
+            _calculatorService = calculatorService;
+        }
+
+        [HttpGet]
+        [ActionName("get")]
+        public IHttpActionResult Get(int id)
+        {
+            var result = _calculatorService.Add(id, id);
+            if (result == 0)
+                return NotFound();
+
+            return Ok(result);
+        }
+
+
+        //[HttpPost]
+        //[Route("")]
+        //public IHttpActionResult CreateUser(User user)
+        //{
+        //    if (user == null || !ModelState.IsValid)
+        //        return BadRequest(ModelState);
+
+        //    var createdUser = _userService.CreateUser(user);
+        //    return CreatedAtRoute("GetUserById", new { id = createdUser.Id }, createdUser);
+        //}
+    }
+}
